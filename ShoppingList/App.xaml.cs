377c40dc2c6645d29f9f0c85143f58e99b8dc0e3
Kayla Johnson <kayla.stankowski@gmail.com>
2025-11-06 16:18:46 -0600

    Set Up UI.

---
+++ ShoppingList/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingList.Views;
+
+namespace ShoppingList;
+
+public partial class App : Application
+{
+    public static string SessionKey = "";
+    
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
